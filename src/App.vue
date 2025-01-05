1787108210dd56911854c26447e07042de8416c9
akshata3113 <akshatashejwal2004@gmail.com>
2025-01-05 13:19:23 +0530

    jobcommit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+// import JobListing from '@/components/JobListing.vue'
+</script>
+
+
+
+<template>
+ <Navbar/>
+ <Hero/>
+ <HomeCards/>
+ <JobListings/>
+ <!-- <JobListing/> -->
+ </template>
